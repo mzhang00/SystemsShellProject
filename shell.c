ef671e04cbc0a1a09e6e078884eab31290f9b78c
Michael Zhang <mzhang00@stuy.edu>
2019-12-04 11:47:26 -0500

    Updated codde to take in user input and exectue the command as long as the command is one word ONLY (for now)

--- shell.c
+++ shell.c
@@ -5,23 +5,38 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <dirent.h>
 
 int main(int argc, char * argv[]){
-    fork();
-    char ** test = ["ls", "-a", "-l"];
-    execvp(test[0], test);
-    // char name[500];
-    // if (argc > 1) {
-    //     strcpy(name, argv[1]);
-    // }else{
-    //     fgets(name, 500, stdin);
-    //     int i = 0;
-    //     while (name[i] != '\n') {
-    //         i++;
-    //     }
-    //     name[i] = '\0';
-    // }
+  while (1){
+    char name[500];
+    if (argc > 1) {
+        strcpy(name, argv[1]);
+    }else{
+        fgets(name, 500, stdin);
+        int i = 0;
+        while (name[i] != '\n') {
+            i++;
+        }
+        name[i] = '\0';
+    }
+
     // printf("%s\n", name);
-    return 0;
+
+
+    if (fork() == 0){
+      char * test[100] = {name, NULL};
+      execvp(test[0], test);
+    }
+
+
+    // int status;
+    // wait(&status);
+    // exit(0);
+  }
+  // char * test[100] = {"ls",NULL};
+  // execvp(test[0], test);
+  return 0;
 }
